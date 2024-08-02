1ad6b64c975edffd5e4d87dddd53f6faca86de0d
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-08-02 19:51:35 -0300

    some changes to the config

--- lua/plugins/code_stats.lua
+++ lua/plugins/code_stats.lua
@@ -1,7 +1,7 @@
-return{
-  -- "https://gitlab.com/code-stats/code-stats-vim",
-  -- Lazy = true,
-  -- config = function ()
-  --   codestats_api_key = "SFMyNTY.VTI5bWFYTjBhV052IyNNak0wTVRJPQ.7mRxe7mM6HjODVimnP9cEB8XSAiPBm8JjwXrXIBDAJU"
-  -- end
-}
+-- return{
+  -- -- "https://gitlab.com/code-stats/code-stats-vim",
+  -- -- Lazy = true,
+  -- -- config = function ()
+  -- --   codestats_api_key = "SFMyNTY.VTI5bWFYTjBhV052IyNNak0wTVRJPQ.7mRxe7mM6HjODVimnP9cEB8XSAiPBm8JjwXrXIBDAJU"
+  -- -- end
+-- }
