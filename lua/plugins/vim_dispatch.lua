aa0d528a5d09d724a28365c4655010fc308ebf3f
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-08-07 11:18:41 -0300

    vim_dispatch should start when i try to use it's cmd

--- lua/plugins/vim_dispatch.lua
+++ lua/plugins/vim_dispatch.lua
@@ -1,3 +1,3 @@
 return {
-  { 'radenling/vim-dispatch-neovim', lazy = true, cmd = "Dispatch", dependencies = { 'tpope/vim-dispatch' } },
+  { 'radenling/vim-dispatch-neovim', lazy = true, cmd = { 'Dispatch', 'Make', 'Copen' }, dependencies = { 'tpope/vim-dispatch' } },
 }
