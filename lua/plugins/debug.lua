da200f34a8283d52e716a87cf39145f434f60594
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-08-08 11:29:37 -0300

    chaging folding method

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -14,6 +14,10 @@ return {
   dependencies = {
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
+    {
+      'theHamsta/nvim-dap-virtual-text',
+      opts = {},
+    },
 
     -- Required dependency for nvim-dap-ui
     'nvim-neotest/nvim-nio',
@@ -92,7 +96,7 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        -- 'delve',
       },
     }
 
