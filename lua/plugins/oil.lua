da5691d23420c2c7ea38c179fb7862b44915cd0b
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-09-12 12:49:43 -0300

    adding new keymaps to oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,15 +2,31 @@ return {
   'stevearc/oil.nvim',
   lazy = true,
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  cmd = 'TryOil',
   opts = {
     delete_to_trash = true,
-    default_file_explorer = true,
     skip_confirm_for_simple_edits = true,
     watch_for_changes = false,
     view_options = {
       show_hidden = true,
-    }
+    },
+    keymaps = {
+      ['gw'] = {
+        function()
+          require('oil').save(nil, nil)
+        end,
+        desc = 'Save Oil Buffer',
+      },
+      ['<leader>sf'] = {
+        function()
+          require('telescope.builtin').find_files {
+            cwd = require('oil').get_current_dir(),
+          }
+        end,
+        mode = 'n',
+        nowait = true,
+        desc = 'Find files in the current oil directory',
+      },
+    },
   },
   keys = {
     { '-', '<cmd>Oil<cr>', desc = 'Open parent directory' },
