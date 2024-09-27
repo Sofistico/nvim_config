67aab2328b0473fd11185c059d926889757e68df
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-09-27 19:13:43 -0300

    adding new plugin for code_stats

--- lua/plugins/waka.lua
+++ lua/plugins/waka.lua
@@ -1,3 +1,14 @@
-return { 'wakatime/vim-wakatime', event = 'BufAdd', keys = {
-  { '<leader>xW', '<cmd>WakaTimeToday<cr>', desc = 'Show Waka Time Today' },
-} }
+return {
+  'wakatime/vim-wakatime',
+  event = 'BufAdd',
+  keys = {
+    { '<leader>xW', '<cmd>WakaTimeToday<cr>', desc = 'Show Waka Time Today' },
+  },
+  {
+    'YannickFricke/codestats.nvim',
+    event = 'BufAdd',
+    opts = {
+      token = 'SFMyNTY.VTI5bWFYTjBhV052IyNNak0wTVRJPQ.7mRxe7mM6HjODVimnP9cEB8XSAiPBm8JjwXrXIBDAJU',
+    },
+  },
+}
