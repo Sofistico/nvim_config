ba7c58d73ed9651c4b90837047f9f3c8a4c9e404
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-09-30 17:31:45 -0300

    changing some stuff on cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -57,10 +57,8 @@ return {
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
-          ['<CR>'] = cmp.mapping.confirm { select = true },
-          ['<Tab>'] = cmp.mapping.confirm { select = true },
-          -- ['<Tab>'] = cmp.mapping.select_next_item(),
-          -- ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ['<CR>'] = cmp.mapping.confirm { select = true, behavior = cmp.ConfirmBehavior.Insert },
+          ['<Tab>'] = cmp.mapping.confirm { select = true, behavior = cmp.ConfirmBehavior.Insert },
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
