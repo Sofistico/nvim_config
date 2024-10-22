d8b100b2218a2f560858b019083836f4a0c54aef
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-10-22 19:14:53 -0300

    new plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -96,11 +96,11 @@ return {
       vim.keymap.set({ 'v', 'n' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      -- vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sl', builtin.resume, { desc = '[S]earch Last Telescope' })
       vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>sG', builtin.git_status, {desc = '[S]earch [G]it Status'})
+      vim.keymap.set('n', '<leader>sG', builtin.git_status, { desc = '[S]earch [G]it Status' })
+      vim.keymap.set('n', '<leader>sc', builtin.colorscheme, { desc = '[S]earch [C]olorscheme' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader><leader>', function()
