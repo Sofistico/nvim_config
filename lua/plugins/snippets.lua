ec927d69d63e2e83c01d175e678364df9ea40626
Sofistico <joaomegamem@gmail.com>
2024-10-24 17:00:15 -0300

    new comments

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -70,6 +70,7 @@ return {
         'rafamadriz/friendly-snippets',
         config = function()
           require('luasnip.loaders.from_vscode').lazy_load()
+          require("luasnip").filetype_extend("cs", { "csharpdoc" })
         end,
       },
     },
