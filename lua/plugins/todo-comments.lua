bd947d50ef0353815ddfaac44906155512df0962
Sofistico <joaomegamem@gmail.com>
2024-11-07 08:01:17 -0300

    adding new stuff

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,5 +1,11 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'BufAdd', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'BufAdd',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+    keys = { { '<leader>st', '<cmd>TodoTelescope<cr>', desc = '[S]earch [T]odo' }, silent = true },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
