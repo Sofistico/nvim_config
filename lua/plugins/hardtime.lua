54f8031f2e8ea6d1112343aa0e0d730b5e8b26d5
Sofistico <joaomegamem@gmail.com>
2024-11-12 10:54:34 -0300

    disabling hardtime automatically starting

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -3,7 +3,7 @@ return {
     'm4xshen/hardtime.nvim',
     dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
     lazy = true,
-    event = 'BufAdd',
+    -- event = 'BufAdd',
     opts = {},
     keys = { { '<leader>tH', '<cmd>Hardtime toggle<cr>', desc = 'Toggle hardtime', silent = true } },
   },
