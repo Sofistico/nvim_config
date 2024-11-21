a0c0a210784a5c91c6ed89fd9c65bd668cffb477
Sofistico <joaomegamem@gmail.com>
2024-11-21 16:44:00 -0300

    removing keymap from vim dispatch since there are default mapping

--- lua/plugins/vim_dispatch.lua
+++ lua/plugins/vim_dispatch.lua
@@ -4,6 +4,6 @@ return {
     lazy = true,
     cmd = { 'Dispatch', 'Make', 'Copen' },
     dependencies = { 'tpope/vim-dispatch' },
-    keys = { { '<leader>cM', '<cmd>Make<cr>', desc = 'Make file/folder/project' } },
+    -- keys = { { '<leader>cM', '<cmd>Make<cr>', desc = 'Make file/folder/project' } },
   },
 }
