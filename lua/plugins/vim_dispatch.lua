d30f98afb530cf657555a709f9d72a9c853a0340
Sofistico <joaomegamem@gmail.com>
2024-11-21 15:50:27 -0300

    new dispatch for make

--- lua/plugins/vim_dispatch.lua
+++ lua/plugins/vim_dispatch.lua
@@ -1,3 +1,9 @@
 return {
-  { 'radenling/vim-dispatch-neovim', lazy = true, cmd = { 'Dispatch', 'Make', 'Copen' }, dependencies = { 'tpope/vim-dispatch' } },
+  {
+    'radenling/vim-dispatch-neovim',
+    lazy = true,
+    cmd = { 'Dispatch', 'Make', 'Copen' },
+    dependencies = { 'tpope/vim-dispatch' },
+    keys = { { '<leader>cM', '<cmd>Make<cr>', desc = 'Make file/folder/project' } },
+  },
 }
