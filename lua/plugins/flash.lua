bc4185a8d41ae71c62809505171bdff6bfea1207
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2024-12-04 19:50:21 -0300

    flash option

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -5,7 +5,9 @@ return {
   {
     'folke/flash.nvim',
     vscode = true,
-    opts = {},
+    opts = { jump = {
+      autojump = true,
+    } },
     -- stylua: ignore
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
