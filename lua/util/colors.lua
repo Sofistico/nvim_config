8633545c7ffe888024ed1dabd5add966ad832f3c
Sofistico <joaomegamem@gmail.com>
2024-12-20 08:48:13 -0300

    changing enum color

--- lua/util/colors.lua
+++ lua/util/colors.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.apply_custom_colors()
   -- csharp exclusive
-  vim.cmd [[hi @lsp.type.enum.cs guifg=LightBlue]]
+  vim.cmd [[hi @lsp.type.enum.cs guifg=SlateBlue]]
   vim.cmd [[hi @lsp.type.property.cs guifg=LightGreen]]
   vim.cmd [[hi @property.c_sharp guifg=LightGreen]]
 end
