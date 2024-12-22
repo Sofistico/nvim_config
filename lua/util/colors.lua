1546358330d53bb887a8c3f2afe4b9959e83abdd
Sofistico <joaomegamem@gmail.com>
2024-12-22 16:27:32 -0300

    lazy lock and color update

--- lua/util/colors.lua
+++ lua/util/colors.lua
@@ -3,8 +3,8 @@ local M = {}
 function M.apply_custom_colors()
   -- csharp exclusive
   vim.cmd [[hi @lsp.type.enum.cs guifg=SlateBlue]]
-  vim.cmd [[hi @lsp.type.property.cs guifg=LightGreen]]
-  vim.cmd [[hi @property.c_sharp guifg=LightGreen]]
+  vim.cmd [[hi @lsp.type.property.cs guifg=DarkCyan]]
+  vim.cmd [[hi @property.c_sharp guifg=DarkCyan]]
 end
 
 return M
