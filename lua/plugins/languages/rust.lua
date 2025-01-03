07b68e0865061a5cc7d307d20c9059cafb07937f
Sofistico <joaomegamem@gmail.com>
2025-01-03 09:00:02 -0300

    lazy lock plus new rust support

---
+++ lua/plugins/languages/rust.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    'Saecki/crates.nvim',
+    event = { 'BufRead Cargo.toml' },
+    opts = {
+      completion = {
+        crates = {
+          enabled = true,
+        },
+      },
+      lsp = {
+        enabled = true,
+        actions = true,
+        completion = true,
+        hover = true,
+      },
+    },
+  },
+  {
+    'nvim-neotest/neotest',
+    optional = true,
+    opts = {
+      adapters = {
+        ['rustaceanvim.neotest'] = {},
+      },
+    },
+  },
+}
