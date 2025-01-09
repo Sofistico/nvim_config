cf40073e5403285bf5b16827d3af11e626059c9d
Sofistico <joaomegamem@gmail.com>
2025-01-09 14:52:43 -0300

    removing word reference navigation

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,7 +6,6 @@ return {
     priority = 1000,
     opts = {
       bufdelete = { enabled = true },
-      words = { enabled = true },
       rename = { enabled = true },
       toggle = { enabled = true },
     },
@@ -24,22 +23,22 @@ return {
         end,
         desc = 'Delete Buffer',
       },
-      {
-        ']]',
-        function()
-          require('snacks').words.jump(vim.v.count1)
-        end,
-        desc = 'Next Reference',
-        mode = { 'n', 't' },
-      },
-      {
-        '[[',
-        function()
-          require('snacks').words.jump(-vim.v.count1)
-        end,
-        desc = 'Prev Reference',
-        mode = { 'n', 't' },
-      },
+      -- {
+      --   ']]',
+      --   function()
+      --     require('snacks').words.jump(vim.v.count1)
+      --   end,
+      --   desc = 'Next Reference',
+      --   mode = { 'n', 't' },
+      -- },
+      -- {
+      --   '[[',
+      --   function()
+      --     require('snacks').words.jump(-vim.v.count1)
+      --   end,
+      --   desc = 'Prev Reference',
+      --   mode = { 'n', 't' },
+      -- },
     },
   },
 }
