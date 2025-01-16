b50b6dfc851d88a044ce6671a458827814e22e41
Sofistico <joaomegamem@gmail.com>
2025-01-16 10:10:20 -0300

    adding nu shell to vim

--- lua/options.lua
+++ lua/options.lua
@@ -124,5 +124,9 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
+if vim.fn.executable 'nu' == 1 then
+  vim.o.shell = 'nu'
+end
+
 require('util.colors').apply_custom_colors()
 -- vim: ts=2 sts=2 sw=2 et
