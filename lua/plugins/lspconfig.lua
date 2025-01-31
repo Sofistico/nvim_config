9ec70052b48ae1915f6cb2fd9c556b6eb378d845
Sofistico <joaomegamem@gmail.com>
2025-01-31 08:54:48 -0300

    stopped from using the ensure installed to drain from server

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -401,7 +401,10 @@ return {
       --  You can press `g?` for help in this menu.
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
+      local ensure_installed = {
+        'lua_ls',
+        'omnisharp',
+      }
       local ensure_installed_tools = {
         'stylua', -- Used to format Lua code
         'csharpier', -- used to format c# code
