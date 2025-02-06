abe2fd1ec5157d14db037a11e30b916fc83faf4d
Sofistico <joaomegamem@gmail.com>
2025-02-06 16:06:11 -0300

    change tabstop to be 2 and shiftwidth 2

--- lua/options.lua
+++ lua/options.lua
@@ -114,10 +114,10 @@ end
 
 vim.g.autoformat = false
 
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = -1 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
 
 -- if vim.fn.executable 'nu' == 1 then
 --   vim.o.shell = 'nu'
