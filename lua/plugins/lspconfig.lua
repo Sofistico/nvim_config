92d912d7c7ed6b273b6e96da7ede179e03343a13
Sofistico <joaomegamem@gmail.com>
2025-02-07 11:38:36 -0300

    putting in on general stuff

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -101,6 +101,10 @@ return {
             title = 'signature helper',
           })
 
+          -- Execute a code action, usually your cursor needs to be on top of an error
+          -- or a suggestion from your LSP for this to activate.
+          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+
           -- TODO: Make this take an input like vim.lsp.buf.rename for the rename of the file, see https://github.com/neovim/neovim/blob/f72dc2b4c805f309f23aff62b3e7ba7b71a554d2/runtime/lua/vim/lsp/buf.lua#L319C1-L320C1
           map('<leader>cR', function()
             require('snacks').rename.rename_file()
@@ -134,12 +138,6 @@ return {
                   vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
                 end,
               })
-            end
-
-            if client.supports_method(vim.lsp.protocol.Methods.textDocument_codeAction) then
-              -- Execute a code action, usually your cursor needs to be on top of an error
-              -- or a suggestion from your LSP for this to activate.
-              map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
             end
 
             if client.supports_method(vim.lsp.protocol.Methods.textDocument_rename) then
