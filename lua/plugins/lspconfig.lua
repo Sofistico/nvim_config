1c442cd3ec3948bdcfdfe777ba029ddf59dc111d
Sofistico <joaomegamem@gmail.com>
2025-02-10 14:14:39 -0300

    enabling inlay hints for lua if i want to use it

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -329,6 +329,7 @@ return {
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
+              hint = { enable = true },
             },
           },
         },
