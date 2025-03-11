6bbb9243d9be96796a2c27ef7267e667d317033d
Sofistico <joaomegamem@gmail.com>
2025-03-11 09:50:54 -0300

    removing tiny inline diagnostic, adding a html linter, lazy locn and typer keymaps

--- lua/plugins/typer.lua
+++ lua/plugins/typer.lua
@@ -1,7 +1,18 @@
-return
-{
-    "nvzone/typr",
-    dependencies = "nvzone/volt",
-    opts = {},
-    cmd = { "Typr", "TyprStats" },
+return {
+  'nvzone/typr',
+  dependencies = 'nvzone/volt',
+  opts = {},
+  cmd = { 'Typr', 'TyprStats' },
+  keys = {
+    {
+      '<leader>Nt',
+      '<cmd>Typr<cr>',
+      desc = 'Typr'
+    },
+    {
+      '<leader>NT',
+      '<cmd>TyprStats<cr>',
+      desc = 'Typr Stats'
+    },
+  },
 }
