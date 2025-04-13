0b683cb3c5d352d329237b69613211d7a7666496
Sofistico <joaomegamem@gmail.com>
2025-04-13 15:45:25 -0300

    removing using that doesnt work quite right

--- lua/plugins/languages/roslyn.lua
+++ lua/plugins/languages/roslyn.lua
@@ -2,41 +2,6 @@ local lsp = require 'util.self_lsp'
 local helpers = require 'util.self_init'
 local selected_project = nil
 
-local function remove_unused_using()
-  vim.api.nvim_create_user_command('RoslynRemoveUsing', function()
-    local bufnr = vim.api.nvim_get_current_buf()
-
-    local clients = vim.lsp.get_clients { name = 'roslyn' }
-    if not clients or vim.tbl_isempty(clients) then
-      vim.notify("Couldn't find client", vim.log.levels.ERROR, { title = 'Roslyn' })
-      return
-    end
-
-    local client = clients[1]
-    local action = {
-      kind = 'quickfix',
-      data = {
-        CustomTags = { 'RemoveUnnecessaryImports' },
-        TextDocument = { uri = vim.uri_from_bufnr(bufnr) },
-        CodeActionPath = { 'Remove unnecessary usings' },
-        Range = {
-          ['start'] = { line = 0, character = 0 },
-          ['end'] = { line = 0, character = 0 },
-        },
-        UniqueIdentifier = 'Remove unnecessary usings',
-      },
-    }
-
-    client.request('codeAction/resolve', action, function(err, resolved_action)
-      if err then
-        vim.notify('Fix using directives failed', vim.log.levels.ERROR, { title = 'Roslyn' })
-        return
-      end
-      vim.lsp.util.apply_workspace_edit(resolved_action.edit, client.offset_encoding)
-    end)
-  end, { desc = 'Remove unnecessary using directives' })
-end
-
 return {
   {
     'seblyng/roslyn.nvim',
@@ -202,7 +167,6 @@ return {
           })
         end,
       })
-      remove_unused_using()
     end,
   },
 }
