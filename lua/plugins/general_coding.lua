8d966e10b78d4a776f18c7ea231943336fd0cd03
Sofistico <joaomegamem@gmail.com>
2025-05-29 12:09:51 -0300

    removing todo comment plugin

--- lua/plugins/general_coding.lua
+++ lua/plugins/general_coding.lua
@@ -6,13 +6,13 @@ return -- snippets
     event = 'BufAdd',
     lazy = true,
   },
-  {
-    'folke/todo-comments.nvim',
-    event = 'BufAdd',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
-    keys = { { '<leader>st', '<cmd>TodoTelescope<cr>', desc = '[S]earch [T]odo' }, silent = true },
-  },
+  -- {
+  --   'folke/todo-comments.nvim',
+  --   event = 'BufAdd',
+  --   dependencies = { 'nvim-lua/plenary.nvim' },
+  --   opts = { signs = false },
+  --   keys = { { '<leader>st', '<cmd>TodoTelescope<cr>', desc = '[S]earch [T]odo' }, silent = true },
+  -- },
   -- library used by other plugins
   { 'nvim-lua/plenary.nvim', lazy = true },
 }
