2cdc530a86ca21d580556a6b9efda57079f6691e
Sofistico <joaomegamem@gmail.com>
2025-06-12 09:58:45 -0300

    lazy lock

--- ftplugin/cs.lua
+++ ftplugin/cs.lua
@@ -2,6 +2,29 @@ vim.cmd 'comp! dotnet'
 vim.cmd 'setlocal et sw=4'
 vim.bo.mp = 'dotnet build --nologo /clp:NoSummary'
 
+local dotnet = require 'easy-dotnet'
+
+vim.keymap.set('n', '<leader>cn', function()
+  dotnet.new()
+end, { desc = 'New Dotnet item', silent = true, buffer = true })
+
+vim.keymap.set('n', '<leader>cs', function()
+  dotnet.secrets()
+end, { desc = 'Dotnet secrets', silent = true, buffer = true })
+
+-- vim.keymap.set('n', '<leader>cN', '<cmd>DotnetUI file bootstrap<cr>', { desc = 'Bootstrap new file', silent = true, buffer = true })
+vim.keymap.set('n', '<leader>cp', function()
+  dotnet.add_package()
+end, { desc = 'Add new nuget package', silent = true, buffer = true })
+
+vim.keymap.set('n', '<leader>cP', function()
+  dotnet.project_view()
+end, { desc = 'Project view', silent = true, buffer = true })
+
+vim.keymap.set('n', '<leader>cS', function()
+  dotnet.solution_select()
+end, { desc = 'Select solution', silent = true, buffer = true })
+
 vim.keymap.set('n', '<leader>te', function()
   vim.g.dotnet_errors_only = not vim.g.dotnet_errors_only
   vim.notify('Toggled dotnet errors: ' .. tostring(vim.g.dotnet_errors_only))
--- lua/plugins/languages/csharp.lua
+++ lua/plugins/languages/csharp.lua
@@ -214,27 +214,6 @@ return {
         picker = 'telescope',
         background_scanning = true,
       }
-
-      vim.keymap.set('n', '<leader>cn', function()
-        dotnet.new()
-      end, { desc = 'New Dotnet item', silent = true, buffer = true })
-
-      vim.keymap.set('n', '<leader>cs', function()
-        dotnet.secrets()
-      end, { desc = 'Dotnet secrets', silent = true, buffer = true })
-
-      -- vim.keymap.set('n', '<leader>cN', '<cmd>DotnetUI file bootstrap<cr>', { desc = 'Bootstrap new file', silent = true, buffer = true })
-      vim.keymap.set('n', '<leader>cp', function()
-        dotnet.add_package()
-      end, { desc = 'Add new nuget package', silent = true, buffer = true })
-
-      vim.keymap.set('n', '<leader>cP', function()
-        dotnet.project_view()
-      end, { desc = 'Project view', silent = true, buffer = true })
-
-      vim.keymap.set('n', '<leader>cS', function()
-        dotnet.solution_select()
-      end, { desc = 'Select solution', silent = true, buffer = true })
     end,
   },
 }
