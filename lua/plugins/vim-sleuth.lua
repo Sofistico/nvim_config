d78cf878b7aabf2c8b73c0b6f31136143b2bdbf9
Sofistico <joaomegamem@gmail.com>
2025-06-12 19:15:54 -0300

    clean up of files and switching from sleuth to guess-ident

--- lua/plugins/vim-sleuth.lua
+++ lua/plugins/vim-sleuth.lua
@@ -1,5 +1,6 @@
 return {
-  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  --'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   lazy = true,
   event = 'BufAdd',
 }
