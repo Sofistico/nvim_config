5497d3d3615478e5cbdb08e663c613bcb2b12cf0
Sofistico <joaomegamem@gmail.com>
2025-07-19 15:49:22 -0300

    lazy lock and new autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -119,3 +119,17 @@ if vim.fn.filereadable(vim.fn.getcwd() .. '/project.godot') == 1 then
   vim.fn.serverstart(addr)
   vim.notify('Neovim server started on ' .. addr)
 end
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuOpen",
+  callback = function()
+    vim.b.copilot_suggestion_hidden = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuClose",
+  callback = function()
+    vim.b.copilot_suggestion_hidden = false
+  end,
+})
