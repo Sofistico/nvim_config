648f1e6ea8b5720a104315fa940b01e6c020d896
Sofistico <joaomegamem@gmail.com>
2025-11-29 20:29:33 -0300

    change to autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -5,28 +5,22 @@ return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   lazy = true,
-  opts = function()
-    local npairs = require 'nvim-autopairs'
-    local Rule = require 'nvim-autopairs.rule'
-    local cond = require 'nvim-autopairs.conds'
-
-    npairs.setup {
-      fast_wrap = {
-        map = '<M-e>',
-        chars = { '{', '[', '(', '"', "'" },
-        pattern = [=[[%'%"%>%]%)%}%,]]=],
-        end_key = '$',
-        before_key = 'h',
-        after_key = 'l',
-        cursor_pos_before = true,
-        avoid_move_to_end = true, -- stay for direct end_key use
-        keys = 'qwertyuiopzxcvbnmasdfghjkl',
-        manual_position = true,
-        highlight = 'Search',
-        highlight_grey = 'Comment',
-      },
-      check_ts = true,
-      enable_check_bracket_line = false,
-    }
-  end,
+  opts = {
+    fast_wrap = {
+      map = '<M-e>',
+      chars = { '{', '[', '(', '"', "'" },
+      pattern = [=[[%'%"%>%]%)%}%,]]=],
+      end_key = '$',
+      before_key = 'h',
+      after_key = 'l',
+      cursor_pos_before = true,
+      avoid_move_to_end = true, -- stay for direct end_key use
+      keys = 'qwertyuiopzxcvbnmasdfghjkl',
+      manual_position = true,
+      highlight = 'Search',
+      highlight_grey = 'Comment',
+    },
+    check_ts = true,
+    enable_check_bracket_line = false,
+  },
 }
